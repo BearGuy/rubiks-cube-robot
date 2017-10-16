b00d1615bd5887fddb616766267d1996bd407d5c
BearGuy <stephen@peterkins.ca>
2017-10-16 20:03:33 +0000

    fixed motor code

--- motor/motor.py
+++ motor/motor.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO            # import RPi.GPIO module  
 from time import sleep             # lets us have a delay  
-GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output   
-GPIO.setup(23, GPIO.OUT)           # set GPIO23 as an output   
+GPIO.setmode(GPIO.BOARD)             # choose BCM or BOARD  
+GPIO.setup(3, GPIO.OUT)           # set GPIO24 as an output   
+GPIO.setup(5, GPIO.OUT)           # set GPIO23 as an output   
  
 ## legend
 # MB = Back motor
@@ -18,25 +18,28 @@ GPIO.setup(23, GPIO.OUT)           # set GPIO23 as an output
 # MBL_dir_pin = # the number of the raspberry pi pin connected to the dir pin on the motor 1 (linear) driver
 # dist= # number of steps required to translate arm adequate distance
 
-MB_step_pin = # the number of the raspberry pi pin connected to step pin on motor driver 
-MB_dir_pin = # the number of the raspberry pi pin connected to the dir pin on the motor driver
-# intialize MB step?
+MB_step_pin = 5 # the number of the raspberry pi pin connected to step pin on motor driver 
+MB_dir_pin = 3 # the number of the raspberry pi pin connected to the dir pin on the motor driver
 
-def MB_rot(MB_step,degrees,dir): #dir input should be 1 for CW, 0 for CCW, entres degrees in degrees of rotation
+def MB_rot(degrees,direct): #dir input should be 1 for CW, 0 for CCW, entres degrees in degrees of rotation
   
-  GPIO.output(MB_dir_pin,dir) # set direction for motor to drive, 1 = CW
+  GPIO.output(MB_dir_pin,direct) # set direction for motor to drive, 1 = CW
 
-  steps=(degrees/1.8)-1
-
-  for i in range (0:steps):
-    if MB_step == 1: 
-      GPIO.output(MB_step_pin,0)
-      MB_step=0
-    else:
+  steps=(degrees/1.8)
+  MB_step = 0
+    
+  steps = int(steps)
+  for i in range(steps):
+    if MB_step == 0: 
       GPIO.output(MB_step_pin,1)
       MB_step=1
+    else:
+      MB_step=0
+      GPIO.output(MB_step_pin,0)
+    print("step {}".format(i))
+    sleep(0.1)
 
-  return MB_step
+MB_rot(36000,1)
 
 
 GPIO.cleanup()                 # resets all GPIO ports used by this program  
