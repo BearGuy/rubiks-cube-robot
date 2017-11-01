17c3e29400696835495515614835299a44b79401
BearGuy <stephen@peterkins.ca>
2017-11-01 18:46:38 +0000

    create separate Motor and Linear classes and remove comments

--- solve/motor.py
+++ solve/motor.py
@@ -1,31 +1,10 @@
-from time import sleep             # lets us have a delay
-import RPi.GPIO as GPIO            # import RPi.GPIO module
+from time import sleep
+import RPi.GPIO as GPIO
 
-## legend
-# MB = Back motor
-# MBL = Translational back motor
-# MR = Right motor
-# MRL = Translational right motor
-# ML = Left Motor
-# MLL = Translational left motor
-# MF = Front Motor
-# MFL = Translational front motor
-
-## Back Motors
-
-# MBL_step_pin = the number of the raspberry pi pin connected to step pin on the motor 1 (linear) driver
-# MBL_dir_pin = the number of the raspberry pi pin connected to the dir pin on the motor 1 (linear) driver
-
-# MB_step_pin = 5 # the number of the raspberry pi pin connected to step pin on motor driver
-# MB_dir_pin = 3 # the number of the raspberry pi pin connected to the dir pin on the motor driver
-
-# dist = # number of steps required to translate arm adequate distance
-# dir input should be 1 for CW, 0 for CCW, entres degrees in degrees of rotation
-
-class Motor:
-    """Create Motor Object"""
+class Stepper:
+    """Create Stepper Object"""
     def __init__(self, role, step_pin, dir_pin):
-        GPIO.setmode(GPIO.BOARD)             # choose BCM or BOARD
+        GPIO.setmode(GPIO.BOARD)  # choose BCM or BOARD
         GPIO.setup(dir_pin, GPIO.OUT)
         GPIO.setup(step_pin, GPIO.OUT)
 
@@ -34,13 +13,13 @@ class Motor:
         self.dir_pin = dir_pin
         self.degrees = 0
 
-    """Rotates a side of the cube given the degrees and direction inputted"""
+    """Rotates the stepper motor given the degrees inputted"""
     def rotate(self, degrees):
         if degrees >= 0:
             direct = 1
         else:
             direct = 0
-        GPIO.output(self.dir_pin, direct) # set direction for motor to drive, 1 = CW
+        GPIO.output(self.dir_pin, direct) # set direction for motor to turn, 1 = CW
 
         steps = int(abs(degrees)/1.8)
 
@@ -53,12 +32,17 @@ class Motor:
 
         self.degrees += degrees
 
-    """Resets the claw attached to the motor"""
+    """Resets the to the motor to 0 degrees"""
     # def reset(self):
         # self.degrees = self.degrees % 360
         # self.rotate(-self.degrees) # rotate backwards to starting position
         # self.degrees = 0
 
-class Linear:
-    def __init__(self):
+
+class Motor(Stepper):
+    def new_func(self):
+        return
+
+class Linear(Stepper):
+    def new_func(self):
         return
