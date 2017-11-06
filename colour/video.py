4750e4054a8a2cd32e12fae6a50b269d08bf1af0
BearGuy <stephen@peterkins.ca>
2017-11-06 22:31:10 +0000

    add headless scan function

--- colour/video.py
+++ colour/video.py
@@ -9,7 +9,7 @@ class Video:
         self.cam              = cv2.VideoCapture(0)
         self.stickers         = self.get_sticker_coordinates('main')
         self.current_stickers = self.get_sticker_coordinates('current')
-        self.preview_stickers = self.get_sticker_coordinates('preview')
+        self.postview_stickers = self.get_sticker_coordinates('postview')
         self.colour = ColourDetection()
 
     def get_sticker_coordinates(self, name):
@@ -32,7 +32,7 @@ class Video:
                 [20, 54], [54, 54], [88, 54],
                 [20, 88], [54, 88], [88, 88]
             ],
-            'preview': [
+            'postview': [
                 [20, 130], [54, 130], [88, 130],
                 [20, 164], [54, 164], [88, 164],
                 [20, 198], [54, 198], [88, 198]
@@ -51,9 +51,9 @@ class Video:
         for index,(x,y) in enumerate(self.current_stickers):
             cv2.rectangle(frame, (x,y), (x+32, y+32), self.colour.name_to_rgb(state[index]), -1)
 
-    def draw_preview_stickers(self, frame, state):
-        """Draws the 9 preview stickers in the frame."""
-        for index,(x,y) in enumerate(self.preview_stickers):
+    def draw_postview_stickers(self, frame, state):
+        """Draws the 9 postview stickers in the frame."""
+        for index,(x,y) in enumerate(self.postview_stickers):
             cv2.rectangle(frame, (x,y), (x+32, y+32), self.colour.name_to_rgb(state[index]), -1)
 
     def color_to_notation(self, color):
@@ -79,7 +79,7 @@ class Video:
     def scan(self):
         """
         Open up the webcam and scans the 9 regions in the center
-        and show a preview in the left upper corner.
+        and show a postview in the left upper corner.
 
         After hitting the space bar to confirm, the block below the
         current stickers shows the current state that you have.
@@ -89,12 +89,16 @@ class Video:
         """
 
         sides   = {}
-        preview = ['white','white','white',
+        postview = ['white','white','white',
                    'white','white','white',
                    'white','white','white']
-        state   = [0,0,0,
-                   0,0,0,
-                   0,0,0]
+        # state   = [0,0,0,
+        #            0,0,0,
+        #            0,0,0]
+        state = ['white','white','white',
+                   'white','white','white',
+                   'white','white','white']
+
         while True:
             _, frame = self.cam.read()
             #frame = cv2.flip(frame,0)
@@ -103,7 +107,7 @@ class Video:
 
             # init certain stickers.
             self.draw_main_stickers(frame)
-            self.draw_preview_stickers(frame, preview)
+            self.draw_postview_stickers(frame, postview)
 
             for index,(x,y) in enumerate(self.stickers):
                 roi          = hsv[y:y+32, x:x+32]
@@ -113,8 +117,8 @@ class Video:
 
             # update when space bar is pressed.
             if key == 32:
-                preview = list(state)
-                self.draw_preview_stickers(frame, state)
+                postview = list(state)
+                self.draw_postview_stickers(frame, state)
                 face = self.color_to_notation(state[4])
                 notation = [self.color_to_notation(color) for color in state]
                 sides[face] = notation
@@ -135,6 +139,70 @@ class Video:
 
         self.cam.release()
         cv2.destroyAllWindows()
+        if len(sides) == 6:
+            return sides
+        else:
+            return False
+
+    def scan_headless(self):
+        """
+        Open up the webcam and scans the 9 regions in the center
+        and show a postview in the left upper corner.
+
+        After hitting the space bar to confirm, the block below the
+        current stickers shows the current state that you have.
+        This is show every user can see what the computer toke as input.
+
+        :returns: dictionary
+        """
+
+        sides   = {}
+        postview = ['white','white','white',
+                   'white','white','white',
+                   'white','white','white']
+        state   = [0,0,0,
+                   0,0,0,
+                   0,0,0]
+        while True:
+            _, frame = self.cam.read()
+            #frame = cv2.flip(frame,0)
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            #key = cv2.waitKey(10) & 0xff
+
+            # init certain stickers.
+            self.draw_main_stickers(frame)
+            self.draw_postview_stickers(frame, postview)
+
+            for index,(x,y) in enumerate(self.stickers):
+                roi          = hsv[y:y+32, x:x+32]
+                avg_hsv      = self.colour.average_hsv(roi)
+                color_name   = self.colour.get_color_name(avg_hsv)
+                state[index] = color_name
+
+            # update when space bar is pressed.
+            if condition:  # need to determine when to save cube face
+                postview = list(state)
+                self.draw_postview_stickers(frame, state)
+                face = self.color_to_notation(state[4])  # get center colour
+                notation = [self.color_to_notation(color) for color in state]
+                sides[face] = notation
+
+            # show the new stickers
+            #self.draw_current_stickers(frame, state)
+
+            # append amount of scanned sides
+            #text = 'scanned sides: {}/6'.format(len(sides))
+            #cv2.putText(frame, text, (20, 460), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+
+            # quit on escape.
+            # if key == 27:
+                # break
+
+            # show result
+            # cv2.imshow("default", frame)
+
+        self.cam.release()
+        #cv2.destroyAllWindows()
         if len(sides) == 6:
             return sides
         else:
